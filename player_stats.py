b5b114b54c9bc8aa0bd1a404c870e26a2c5ee670
Harpaul Sidhu <dev.fork@sl.hspro.me>
2024-07-25 13:05:21 -0700

    Implement player stats

---
+++ player_stats.py
@@ -0,0 +1,11 @@
+from nba_api.stats.endpoints import playercareerstats
+
+
+def player_stats(id):
+    recent_season_stats = playercareerstats.PlayerCareerStats(player_id=id).get_data_frames()[0]
+
+    if not recent_season_stats.empty:
+        return recent_season_stats.iloc[-1]
+    return 'Player has not played yet'
+
+print(player_stats(203507))
